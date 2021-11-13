1909a1b502cb8ce813fb11429d07b9542c53a388
Vladimir Alinsky <adam3iks@gmail.com>
2021-11-13 19:41:12 +0300

    Add signal and personal section, remove youtube

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { makeStyles } from '@mui/styles';
@@ -8,16 +7,18 @@ import {
   Box,
   Button,
   Container,
+  Snackbar,
   Paper,
   Typography
 } from "@mui/material";
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
 
 import { ReactComponent as TikTokIcon } from './tiktok.svg'
 import { ReactComponent as VKIcon } from './vk.svg'
+import { ReactComponent as SignalIcon } from './signal.svg'
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import FacebookIcon from '@mui/icons-material/Facebook';
-import YouTubeIcon from '@mui/icons-material/YouTube';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TelegramIcon from '@mui/icons-material/Telegram';
@@ -66,14 +67,21 @@ const useStyles = makeStyles({
   }
 })
 
-function MediaButton({text, icon, href="#"}: {text: string; icon: JSX.Element; href?: string}) {
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref,
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+function MediaButton({text, icon, ...props}: {text: string, icon: JSX.Element, href?: string, onClick?: () => void}) {
   return (
     <Button
       variant="outlined"
       color="warning"
       sx={{margin: "8px"}}
       startIcon={icon}
-      href={href}
+      {...props}
     >
       {text}
     </Button>
@@ -82,6 +90,15 @@ function MediaButton({text, icon, href="#"}: {text: string; icon: JSX.Element; h
 
 function App() {
   const classes = useStyles();
+
+  const [openSignalAlert, setOpenSignalAlert] = React.useState(false);
+  const handleCloseSignalAlert = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpenSignalAlert(false);
+  };
+
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
@@ -103,17 +120,30 @@ function App() {
               <Typography variant="h6" sx={{ fontWeight: 'bold' }}>Social medias</Typography>
               <MediaButton text="Instagram" icon={<InstagramIcon />} href="https://www.instagram.com/sky.alin/" />
               <MediaButton text="Twitter" icon={<TwitterIcon />} href="https://twitter.com/AlinskyVladimir" />
-              <MediaButton text="Facebook" icon={<FacebookIcon />} href="https://www.facebook.com/vladimir.alinsky" />
-              <MediaButton text="TikTok" icon={<TikTokIcon fill="#ffa726" />} href="https://vm.tiktok.com/ZSJe8DYu6/" />
               <MediaButton text="VK" icon={<VKIcon fill="#ffa726" />} href="https://vk.com/voavnix" />
-              <MediaButton text="YouTube" icon={<YouTubeIcon />} href="https://www.youtube.com/channel/UCR_YFCXQu5FW-wyZjuEzLjQ" />
-              <MediaButton text="GitHub" icon={<GitHubIcon />} href="https://github.com/SKY-ALIN" />
+              <MediaButton
+                text="TikTok"
+                icon={<TikTokIcon fill="#ffa726" width="1em" height="1em" />}
+                href="https://vm.tiktok.com/ZSJe8DYu6/"
+              />
+              <MediaButton text="Facebook" icon={<FacebookIcon />} href="https://www.facebook.com/vladimir.alinsky" />
+            </Box>
+
+            <Box className={classes.block}>
+              <Typography variant="h6" sx={{ fontWeight: 'bold' }}>Professional</Typography>
               <MediaButton text="LinkedIn" icon={<LinkedInIcon />} href="https://www.linkedin.com/in/alinsky/" />
+              <MediaButton text="GitHub" icon={<GitHubIcon />} href="https://github.com/SKY-ALIN" />
             </Box>
 
             <Box className={classes.block}>
               <Typography variant="h6" sx={{ fontWeight: 'bold' }}>Ways to contact</Typography>
+
               <MediaButton text="Telegram" icon={<TelegramIcon />} href="https://t.me/sky_alin" />
+              <MediaButton
+                text="Signal"
+                icon={<SignalIcon fill="#ffa726" width="1em" height="1em" />}
+                onClick={() => setOpenSignalAlert(true)}
+              />
               <MediaButton text="Mail" icon={<AlternateEmailIcon />} href="mailto:vladimir@alinsky.tech" />
             </Box>
           </Paper>
@@ -127,6 +157,17 @@ function App() {
           </Box>
         </Paper>
       </div>
+
+      <Snackbar
+        open={openSignalAlert}
+        autoHideDuration={2500}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        onClose={handleCloseSignalAlert}
+      >
+        <Alert severity="warning" sx={{ width: '100%' }} onClose={handleCloseSignalAlert}>
+          Available upon personal request only
+        </Alert>
+      </Snackbar>
     </ThemeProvider>
   );
 }
